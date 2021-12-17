609d598e2e0d9a21ebade77954a073c6642a2561
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-12-17 01:16:01 -0500

    formatting + output

--- 03_recursion/02_greet/02_greet.rb
+++ 03_recursion/02_greet/02_greet.rb
@@ -1,18 +1,22 @@
 # Recursion - function calls itself; used when it makes the solution clearer.
 
 def greet(name)
-  p "hello, " + name + "!"
+  p "hello, #{name}!"
   greet2(name)
-  p "getting ready to say bye..."
-  bye()
+  p 'getting ready to say bye...'
+  bye
 end
 
 def greet2(name)
-  p "how are you, " + name + "?"
+  p "how are you, #{name}?"
 end
 
-def bye()
-  p "ok bye!"
+def bye
+  p 'ok bye!'
 end
 
-greet("maggie")
+greet('Tiffany')
+# "hello, Tiffany!"
+# "how are you, Tiffany?"
+# "getting ready to say bye..."
+# "ok bye!"
