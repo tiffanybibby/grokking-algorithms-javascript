0a8e42334f25dbb80099b57fb393579e0730ca30
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-12-17 03:01:42 -0500

    ruby spread syntax

--- 04_quicksort/02_recursive_sum.rb
+++ 04_quicksort/02_recursive_sum.rb
@@ -2,7 +2,7 @@
 
 def sumRecursive(array)
   return 0 if array.empty? # base case
-  array[0] + sumRecursive(array.slice(1..array.length))
+  array[0] + sumRecursive(array.slice(1..-1))
 end
 
 p sumRecursive([1, 2, 3, 4]) # 10
