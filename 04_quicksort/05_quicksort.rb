3a4bfed4dda63351f06f0212fb5a53b189c00c1f
Tiffany Bibby <tiffanybibby@Tiffanys-MBP.fios-router.home>
2021-12-18 23:35:56 -0500

    quicksort js and ruby

---
+++ 04_quicksort/05_quicksort.rb
@@ -0,0 +1,13 @@
+# Quick array sorting
+
+def quicksort(array)
+  return array if array.length < 2
+
+  pivot = array[0]
+  less = array.slice(1..-1).select {|num| num <= pivot}
+  greater = array.slice(1..-1).select {|num| num > pivot}
+
+  return quicksort(less) + [pivot] + quicksort(greater) 
+end
+
+p quicksort([10, 5, 2, 3]); # [2, 3, 5, 10]
